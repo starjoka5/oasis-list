b515784d1a81cacdabdbcb3dc08162b3d6a23ac0
Gilberto Martinez <starjoka5@gmail.com>
2021-07-13 20:46:52 -0500

    prod api url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    API:process.env.API || 'http://localhost:3080/api',
+    API:process.env.API || 'https://oasis-hotels.herokuapp.com/api',
     PUBLIC_URL:'https://api-onow.oasishoteles.net/'
 }
